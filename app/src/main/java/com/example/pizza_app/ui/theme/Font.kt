ec3100cea34ed0c7e4c49f574d2b87a860c7ed1e
Thoraya <thoriahamdy99@gmail.com>
2025-06-13 22:50:40 +0300

    define fonts to the app

---
+++ app/src/main/java/com/example/pizza_app/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.example.pizza_app.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.pizza_app.R
+
+val SFProFont = FontFamily(
+    Font(R.font.sf_pro_bold, weight = FontWeight.Bold),
+    Font(R.font.sf_pro_bold, weight = FontWeight.Medium),
+    Font(R.font.sf_pro_bold, weight = FontWeight.Normal),
+)
